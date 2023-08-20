f037638520c9c81d53feffddd86667ee430b463a
domflp <109469999+domflp@users.noreply.github.com>
2023-08-20 17:51:33 -0300

    Atividade SmartTv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,31 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv  smartTv = new SmartTv();
+
+smartTv.diminuirVolume();
+smartTv.diminuirVolume();
+smartTv.diminuirVolume();
+smartTv.aumentarVolume();
+
+System.out.println("Volume Atual : " + smartTv.volume);
+System.out.println("Canal Atual : "  + smartTv.canal);
+System.out.println("Canal Atual : "  + smartTv.canal);
+
+        System.out.println("Canal Atual : "  + smartTv.canal);  
+
+             System.out.println("TV Ligada? "  + smartTv.ligada);
+         
+        System.out.println("Volume Atual : "  + smartTv.volume);
+
+                smartTv.mudarCanal(13);
+
+                 smartTv.ligar ();
+         System.out.println(" Novo Status ->  Ligada ? "  + smartTv.ligada);
+
+                smartTv.desligar();
+        System.out.println(" Novo Status ->  Ligada ? "  + smartTv.ligada);
+
+
+    }
+}
